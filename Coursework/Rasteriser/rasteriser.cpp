1fbe9d0eebc5780b27e54ba2c687242d61c877d1
Jaydon <jaydon.annobil@mail.bcu.ac.uk>
2026-08-25 07:21:25 +0100

    white triangle
    added filled triangle scanline rasterization.

--- Coursework/Rasteriser/rasteriser.cpp
+++ Coursework/Rasteriser/rasteriser.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <algorithm>
 #include <lodepng.h>
+
+
+// --- draw pixels --
+void drawPixel(std::vector<uint8_t>& imageBuffer, int width, int height, int nChannels,
+    int x, int y, uint8_t r, uint8_t g, uint8_t b)
+{
+    // --- stops crashes if it tries to draw outside image --
+    if (x < 0 || x >= width || y < 0 || y >= height)
+        return;
+
+    int pixelIdx = x + y * width;
+
+    imageBuffer[pixelIdx * nChannels + 0] = r;
+    imageBuffer[pixelIdx * nChannels + 1] = g;
+    imageBuffer[pixelIdx * nChannels + 2] = b;
+    imageBuffer[pixelIdx * nChannels + 3] = 255;
+}
 
 int main()
 {
@@ -18,10 +36,7 @@ int main()
         {
             int pixelIdx = x + y * width;
 
-            imageBuffer[pixelIdx * nChannels + 0] = 0;
-            imageBuffer[pixelIdx * nChannels + 1] = 0;
-            imageBuffer[pixelIdx * nChannels + 2] = 0;
-            imageBuffer[pixelIdx * nChannels + 3] = 255;
+            drawPixel(imageBuffer, width, height, nChannels, x, y, 0, 0, 0);
         }
     }
 
@@ -42,10 +57,7 @@ int main()
 
                 int pixelIdx = x + y * width;
 
-                imageBuffer[pixelIdx * nChannels + 0] = 255;
-                imageBuffer[pixelIdx * nChannels + 1] = 255;
-                imageBuffer[pixelIdx * nChannels + 2] = 255;
-                imageBuffer[pixelIdx * nChannels + 3] = 255;
+                drawPixel(imageBuffer, width, height, nChannels, x, y, 255, 255, 255);
             }
         };
 
@@ -53,6 +65,24 @@ int main()
     drawLine(x1, y1, x2, y2);
     drawLine(x2, y2, x3, y3);
     drawLine(x3, y3, x1, y1);
+
+    // Fill the triangle using horizontal scanlines
+    for (int y = y1; y <= y2; y++)
+    {
+        float t = float(y - y1) / float(y2 - y1);
+
+        int leftX = x1 + t * (x2 - x1);
+        int rightX = x3 + t * (x2 - x3);
+
+        if (leftX > rightX)
+            std::swap(leftX, rightX);
+
+        for (int x = leftX; x <= rightX; x++)
+        {
+            drawPixel(imageBuffer, width, height, nChannels,
+                x, y, 255, 255, 255);
+        }
+    }
 
     // Save the image
     int errorCode;
